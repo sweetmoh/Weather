601f108fa097962cb9164acdde8d0b89fc4ef71a
sweetmoh <dlaminimongezi78@gmail.com>
2024-08-07 19:29:26 +0200

    setting api variables

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getWeatherData(cityName: String) {
+    this.http.get(environment.weatherApiBaseUrl, {
+      headers : new HttpHeaders()
+      .set(environment.xRapidAPIHostHeaderName, environment.xRapidAPIHostHeaderValue)
+      .set(environment.xRapidApiKeyHeaderName, environment.xRapidApiKeyHeaderValue),
+      params: new HttpParams()
+      .set('city', cityName.toString())
+      .set('lang', 'EN')
+    })
+  }
 }
